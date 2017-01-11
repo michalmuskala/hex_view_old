f1da2cb9a8a34b6989c19c3864e9ad3ba8e399d4
Michal Muskala <michal@muskala.eu>
2017-01-11 16:00:10 +0100

    Add echos to debug logs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,16 @@ set -e
 
 NAME="hex_view"
 VERSION="0.0.1"
-
 export MIX_ENV=prod
+
+echo "Compiling assets"
 (cd assets && npm run deploy)
 mix phoenix.digest
+
+echo "Building release"
 mix release --env=prod
+
+echo "Uploading release"
 scp "_build/$MIX_ENV/rel/$NAME/releases/$VERSION/$NAME.deb" \
     "$DEPLOYER@$SERVER":"$NAME.deb"
 ssh "$DEPLOYER@$SERVER" 'sudo dpkg -i '"$NAME"'.deb'
