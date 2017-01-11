5598f6284358a14898b6891c49e4efc934f7bb5d
Josh Adams <josh.rubyist@gmail.com>
2017-01-10 23:21:38 -0600

    Add markdown support

--- assets/elm/Pages/PackageView/View.elm
+++ assets/elm/Pages/PackageView/View.elm
@@ -1,6 +1,7 @@
-module Pages.PackageView.View exposing
-    ( view
-    )
+module Pages.PackageView.View
+    exposing
+        ( view
+        )
 
 import Bootstrap exposing (..)
 import Data.RootedTree as RootedTree exposing (RootedTree, RootedTreeZipper)
@@ -9,29 +10,37 @@ import Html.Attributes exposing (href)
 import Html.Events exposing (onClick)
 import Pages.PackageView.Model as Model exposing (Model, Files, CurrentFile(..))
 import Pages.PackageView.Update as Update exposing (Msg(..))
+import Markdown
+
 
 view : Model a -> Html Msg
 view model =
     let
-        nav = navbar []
-        content = main_ []
-                  [ segmentedHeading model.name model.version
-                  , renderBreadcrumbs model
-                  , renderTree model.files
-                  , renderCurrentFile model.currentFile
-                  ]
+        nav =
+            navbar []
+
+        content =
+            main_ []
+                [ segmentedHeading model.name model.version
+                , renderBreadcrumbs model
+                , renderTree model.files
+                , renderCurrentFile model.currentFile
+                ]
     in
         div []
             [ nav
             , content
             ]
 
+
 renderBreadcrumbs : Model a -> Html Msg
 renderBreadcrumbs model =
     let
-        crumbs = RootedTree.breadcrumbs model.files model.name
+        crumbs =
+            RootedTree.breadcrumbs model.files model.name
     in
         breadcrumbs GoUp crumbs
+
 
 renderTree : Files -> Html Msg
 renderTree files =
@@ -47,18 +56,28 @@ renderTree files =
                 |> RootedTree.children
                 |> List.indexedMap buildRow
 
-        headers = ["icon", "file", "the end"]
+        headers =
+            [ "icon", "file", "the end" ]
     in
         fileTable headers rows
+
 
 renderCurrentFile : CurrentFile -> Html Msg
 renderCurrentFile file =
     case file of
         Selected name file ->
-            file
-                |> String.lines
-                |> fileContent name
+            case Debug.log "name" name of
+                "README.md" ->
+                    file
+                        |> Markdown.toHtml []
+
+                _ ->
+                    file
+                        |> String.lines
+                        |> fileContent name
+
         Loading ->
             text "loading"
+
         NotSelected ->
             text "no file"
